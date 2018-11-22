e19a4a7a6b05aadee0e227fe070f5050cef455f8
GRX <gaoruixing@yodo1.com>
2018-11-22 11:20:15 +0800

    add event manager

---
+++ Assets/Script/EventManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public static EventManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
