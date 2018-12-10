38e5d30496a11e022bcc0f4ba1b1a565e25c7256
GRX <gaoruixing@yodo1.com>
2018-12-10 18:31:32 +0800

    add plugin for translation

---
+++ Assets/Script/Tools/GameTools.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using I2.Loc;
+public class GameTools{
+    
+    public static string GetTranslation(string _key)
+    {
+        string t_translation;
+
+        if (LocalizationManager.TryGetTermTranslation(_key, out t_translation))
+        {
+            if (string.IsNullOrEmpty(t_translation))
+                t_translation = "#Empty: " + _key;
+            else
+                t_translation = t_translation.Replace("\\n", "\n");
+        }
+        else
+            t_translation = "#" + _key;
+
+        return t_translation;
+    }
+
+}
